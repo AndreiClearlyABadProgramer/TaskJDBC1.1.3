cf009f4f072d10fbe5cf6d07fe15ca8f429de916
Andrei <75860651+AndreiClearlyABadProgramer@users.noreply.github.com>
2021-02-04 22:21:45 +0300

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,31 @@
 package jm.task.core.jdbc.util;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL ="jdbc:mysql://localhost:3306/firstschema?autoReconnect=true&useSSL=false";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "123654978kK@WORDIKS";
+    Connection connection = null;
+    public Connection getConnection(){
+
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Not connected");
+        }
+        return connection;
+    }
+    public void close(){
+        if(connection != null){
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println("Could not close the connection");
+            }
+        }
+    }
 }
