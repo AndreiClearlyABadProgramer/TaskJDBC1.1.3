24ad87669720e87a04252782bb6ea52ba1f2080b
Andrei <75860651+AndreiClearlyABadProgramer@users.noreply.github.com>
2021-02-04 22:21:16 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,22 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService service = new UserServiceImpl();
+        service.createUsersTable();
+        service.saveUser("Andrei", "Dik", (byte)18);
+        service.saveUser("Ivan", "Kirk", (byte)23);
+        service.saveUser("Alex", "Fork", (byte)53);
+        service.saveUser("Dimitri", "Kraig", (byte)60);
+        List<User> users = service.getAllUsers();
+        System.out.println(users);
+        service.cleanUsersTable();
+        service.dropUsersTable();
     }
 }
